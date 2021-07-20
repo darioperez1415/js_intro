904568c7093076a858a735f8f70bd314105c12d4
Dario Perez <darioperez1415@gmail.com>
2021-07-19 19:24:54 -0500

    Added variables in main.js

--- js-intro/main.js
+++ js-intro/main.js
@@ -1,1 +1,6 @@
 console.log("Connected");
+const firstName = "AdazaDGreat";
+const lastName = "Call me anytime";
+const favoriteFood = "Roti";
+const favoriteNumber = 14;
+const likeToPlayFootball = ture;
