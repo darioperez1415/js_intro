73e378a3354b42fbdaf5b544eb50500553423cf9
Dario Perez <darioperez1415@gmail.com>
2021-07-20 18:13:53 -0500

    Added files

--- js-intro/main.js
+++ js-intro/main.js
@@ -1,6 +1,28 @@
-console.log("Connected");
-const firstName = "AdazaDGreat";
-const lastName = "Call me anytime";
+//console.log("Connected");
+//const firstName = "AdazaDGreat";
+//const lastName = "Call me anytime";
 const favoriteFood = "Roti";
-const favoriteNumber = 14;
-const likeToPlayFootball = ture;
+//let favoriteNumber = 14;
+//const likeToPlayFootball = true;
+
+//favoriteNumber += 3;
+
+
+
+
+
+ const whatFoodsDoILike = () => {
+    if (favoriteFood === "hot dogs" || favoriteFood.toUpperCase() === "SUSHI") {
+        console.log("You like hot dogs!");
+     } else {
+         console.log("You like something else!");
+ }
+ }
+
+ whatFoodsDoILike();
+
+ const myFunction = (string) => {
+     console.log(`Your aregument was: ${string}`)
+ }
+
+ myFunction ("I am GREAT!");
