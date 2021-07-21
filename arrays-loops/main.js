1bb10d198f063292a98b7c092b5c3a5e9a6caa2a
Dario Perez <darioperez1415@gmail.com>
2021-07-20 21:34:01 -0500

    JS objects

--- arrays-loops/main.js
+++ arrays-loops/main.js
@@ -1,1 +1,38 @@
-console.log("Connected");
+//Creating objects
+let seudent1 = {    
+name: "Dario",
+email: "darioperez1415@gmail.com",
+favColor: "Ash Grey",
+id: 3,
+unique: "very good at football",
+Image: https://avatars.githubusercontent.com/u/83309084?v=4,
+},
+
+let student2 ={
+name: "Albert",
+email: "albert.w.chittaphong@gmail.com",
+favColor: "red",
+id: 9,
+unique: "black belt",
+Image: https://avatars.githubusercontent.com/u/83558122?v=4,
+},
+
+let student3 = {
+name: "Craig",
+email: "wellspring.craig@gmail.com",
+favColor: "purple",
+id: 1,
+unique: "extremely flexible",
+Image: https://avatars.githubusercontent.com/u/61626143?v=4,
+},
+
+let student4 ={
+name:"Meredith",
+email:"meredith.marcum@hotmail.com",
+favColor: "purple",
+id: 12,
+unique: "Worked on Roller Coaster",
+Image: https://avatars.githubusercontent.com/u/85968205?v=4,
+};
+
+console.log(team4)
