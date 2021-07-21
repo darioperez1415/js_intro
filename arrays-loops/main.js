7f21a569a3cf4e0d8e217f08dfb2aae075c4d5f9
Dario Perez <darioperez1415@gmail.com>
2021-07-21 10:58:22 -0500

    Arrays-loops

--- arrays-loops/main.js
+++ arrays-loops/main.js
@@ -1,5 +1,6 @@
 //Creating objects
-let seudent1 = {    
+let seudent = [
+{    
 name: "Dario",
 email: "darioperez1415@gmail.com",
 favColor: "Ash Grey",
@@ -7,8 +8,7 @@ id: 3,
 unique: "very good at football",
 Image: https://avatars.githubusercontent.com/u/83309084?v=4,
 },
-
-let student2 ={
+{
 name: "Albert",
 email: "albert.w.chittaphong@gmail.com",
 favColor: "red",
@@ -16,23 +16,21 @@ id: 9,
 unique: "black belt",
 Image: https://avatars.githubusercontent.com/u/83558122?v=4,
 },
-
-let student3 = {
-name: "Craig",
+{name: "Craig",
 email: "wellspring.craig@gmail.com",
 favColor: "purple",
 id: 1,
 unique: "extremely flexible",
 Image: https://avatars.githubusercontent.com/u/61626143?v=4,
 },
-
-let student4 ={
+{
 name:"Meredith",
 email:"meredith.marcum@hotmail.com",
 favColor: "purple",
 id: 12,
 unique: "Worked on Roller Coaster",
 Image: https://avatars.githubusercontent.com/u/85968205?v=4,
-};
+}
+];
 
 console.log(team4)
